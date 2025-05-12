1869ae28695deae24bea3257fe5936a16cfb20a9
Noeleen Sisson <noeleenbv@gmail.com>
2025-05-12 14:45:27 +0100

    Minor changes

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,15 @@
-const { purgeCSSPlugin } = require('@fullhuman/postcss-purgecss');
-const autoprefixer = require('autoprefixer'); // Add Autoprefixer
-
+const { purgeCSSPlugin } = require('@fullhuman/postcss-purgecss'); // css clean up
+const autoprefixer = require('autoprefixer'); // cross-browser compatibility
+const cssnano = require('cssnano'); // css minification
 
 module.exports = {
   plugins: [
-    autoprefixer({ // Add Autoprefixer configuration
-        overrideBrowserslist: ['last 2 versions', '> 1%'] // Specify browser support
-      }),
+    autoprefixer({
+      overrideBrowserslist: ['last 2 versions', '> 1%']
+    }),
     purgeCSSPlugin({
       content: ['./**/*.html']
-    })
+    }),
+    cssnano({ preset: 'default' }) // Minify CSS
   ]
 };
